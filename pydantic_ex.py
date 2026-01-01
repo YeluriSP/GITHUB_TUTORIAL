57a24e49143c49f226b52c13f12aff2a5d152598
YeluriSP <prasanthsai311@gmail.com>
2026-01-01 12:07:02 +0530

    first commit

---
+++ pydantic_ex.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+class User(BaseModel):
+    id: int = Field(..., description="The unique identifier for a user")
+    name: str = Field(..., description="The name of the user")
+    email: str = Field(..., description="The email address of the user")
+
+user = User(id=1, name="John Doe", email="john.doe@example.com")
+print(user)
